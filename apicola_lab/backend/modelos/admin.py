c1e12eab8a6a25719b882cbac009a4a474bdbc8f
Lucas G <133524925+Gabitoto@users.noreply.github.com>
2025-06-09 09:45:39 -0300

    se crean los modelos y se conectan con la BBDD al backend

--- apicola_lab/backend/modelos/admin.py
+++ apicola_lab/backend/modelos/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from .models import (
+    Apicultor, Analista, Apiario, Tambor, TamborApiario, Especie, Muestra, MuestraTambor,
+    AnalisisPalinologico, AnalisisFisicoQuimico
+)
 
 # Aquí se registrarán los modelos generados con inspectdb
 # Ejemplo:
 # from .models import TuModelo
 # admin.site.register(TuModelo) 
+
+admin.site.register(Apicultor)
+admin.site.register(Analista)
+admin.site.register(Apiario)
+admin.site.register(Tambor)
+admin.site.register(TamborApiario)
+admin.site.register(Especie)
+admin.site.register(Muestra)
+admin.site.register(MuestraTambor)
+admin.site.register(AnalisisPalinologico)
+admin.site.register(AnalisisFisicoQuimico) 
